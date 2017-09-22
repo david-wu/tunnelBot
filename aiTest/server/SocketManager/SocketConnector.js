46170b2183be4eb9a3e44fe7068840aa5bd68299
David Wu <dwu@mobileiron.com>
2017-09-21 22:10:38 -0700

    dont emit back to origin

--- aiTest/server/SocketManager/SocketConnector.js
+++ aiTest/server/SocketManager/SocketConnector.js
@@ -1,61 +1,62 @@
 /*
-	SocketConnectors are 1:1 with websocket connections
-	They can submit multiple spawn requests and manage multiple child processes
+    SocketConnectors are 1:1 with websocket connections
+    They can submit multiple spawn requests and manage multiple child processes
 */
 
 const _ = require('lodash')
 const axios = require('axios')
-// const Instance = require('./Instance.js')
 const DockerConnector = require('./DockerConnector/DockerConnector.js')
 
 
 class SocketConnector{
 
-	static factory(options){
-		return new SocketConnector(options)
-	}
+    static factory(options){
+        return new SocketConnector(options)
+    }
 
-	constructor(options){
-		_.defaults(this, options)
-		_.defaults(this, {
-			modelEmitter: undefined,
-			dockerConnector: DockerConnector.factory({
-				output: this.ioConnection.send.bind(this.ioConnection)
-			}),
-		})
+    constructor(options){
+        _.defaults(this, options)
+        _.defaults(this, {
+            modelEmitter: undefined,
+            dockerConnector: DockerConnector.factory({
+                output: this.ioConnection.send.bind(this.ioConnection)
+            }),
+        })
 
-		this.ioConnection.on('message', this.messageHandler.bind(this))
-		this.ioConnection.on('disconnect', this.disconnectHandler.bind(this))
-	}
+        this.ioConnection.on('message', this.messageHandler.bind(this))
+        this.ioConnection.on('disconnect', this.disconnectHandler.bind(this))
+    }
 
-	messageHandler(message, callback){
-		if(message.type === 'listen'){
-			this.modelEmitter.on(message.ref, (payload)=>{
-				this.ioConnection.send(message.ref, payload)
-			});
-		}else if(message.type === 'stopListening'){
+    messageHandler(message, callback){
+        if(message.type === 'listen'){
+            this.modelEmitter.on(message.ref, (payload)=>{
+                if(payload.originSocketId === this.ioConnection.id) return
+                this.ioConnection.send(message.ref, payload)
+            });
+        }else if(message.type === 'stopListening'){
 
-		}else if(message.type === 'emit'){
-			this.modelEmitter.emit(message.ref, message.payload)
-		}else if(message.type === 'spawnDir'){
-			this.dockerConnector.spawnDir(message.payload.id)
-				.then(function(instance){
-					callback(false, {
-						id: instance.id
-					})
-				})
-		}else if(message.type === 'stdIn'){
-			this.dockerConnector.sendMessage(message)
-				.then(callback)
-		}else{
-			this.dockerConnector.sendMessage(message)
-				.then(callback)
-		}
-	}
+        }else if(message.type === 'emit'){
+            message.payload.originSocketId = this.ioConnection.id;
+            this.modelEmitter.emit(message.ref, message.payload)
+        }else if(message.type === 'spawnDir'){
+            this.dockerConnector.spawnDir(message.payload.id)
+                .then(function(instance){
+                    callback(false, {
+                        id: instance.id
+                    })
+                })
+        }else if(message.type === 'stdIn'){
+            this.dockerConnector.sendMessage(message)
+                .then(callback)
+        }else{
+            this.dockerConnector.sendMessage(message)
+                .then(callback)
+        }
+    }
 
-	disconnectHandler(){
-		this.dockerConnector.killInstances()
-	}
+    disconnectHandler(){
+        this.dockerConnector.killInstances()
+    }
 
 }
 
